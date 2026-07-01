9228a2241becff43e19e66bc8cf96a4ffbd6cbb2
Huu tam <tam01296275375@gmail.com>
2026-07-02 01:29:31 +0700

    add ss7 ss8 in module 3

--- module3/study_case-1/question21-28.sql
+++ module3/study_case-1/question21-28.sql
@@ -106,3 +106,88 @@ end;
 
 -- 27
 
+-- 27-1
+select * from contract 
+
+
+
+create function func_dem_dich_vu()
+returns int
+reads sql data
+begin 
+	declare v_count int;
+	select count(*) into v_count from contract c
+	where c.deposit_amount >= 2000000;
+	return v_count;
+end;
+
+SELECT func_dem_dich_vu();
+
+
+-- 27-2
+create function func_tinh_thoi_gian_hop_dong(p_customer_id int)
+returns int
+reads sql data
+begin
+    declare v_max_day int;
+
+    select max(datediff(end_date, start_date))
+    into v_max_day
+    from contract
+    where customer_id = p_customer_id;
+
+    return ifnull(v_max_day, 0);
+end;
+
+-- 28 
+
+
+
+create procedure sp_xoa_dich_vu_va_hd_room()
+begin
+
+    -- xóa chi tiết hợp đồng
+    delete from detail_contracts
+    where contract_id in (
+        select contract_id
+        from (
+            select c.contract_id
+            from contract c
+            join services s on c.service_id = s.id
+            join service_type st on s.service_type = st.id
+            where st.name = 'room'
+              and c.start_date between '2015-01-01' and '2019-12-31'
+        ) t
+    );
+
+    -- xóa hợp đồng
+    delete from contract
+    where contract_id in (
+        select contract_id
+        from (
+            select c.contract_id
+            from contract c
+            join services s on c.service_id = s.id
+            join service_type st on s.service_type = st.id
+            where st.name = 'room'
+              and c.start_date between '2015-01-01' and '2019-12-31'
+        ) t
+    );
+
+    -- xóa dịch vụ
+    delete from services
+    where id in (
+        select id
+        from (
+            select s.id
+            from services s
+            join service_type st on s.service_type = st.id
+            join contract c on c.service_id = s.id
+            where st.name = 'room'
+              and c.start_date between '2015-01-01' and '2019-12-31'
+        ) t
+    );
+
+end 
+
+
